b62c914024348253c9fc77ed7cec6781dc0b461b
Bassant Todary <btodary@eserve.com.eg>
2019-03-18 21:48:35 +0200

    Finishing Live data sample

--- app/src/main/java/com/waether/app/features/randomizer/RandomizerActivity.kt
+++ app/src/main/java/com/waether/app/features/randomizer/RandomizerActivity.kt
@@ -1,7 +1,6 @@
 package com.waether.app.features.randomizer
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/waether/app/features/randomizer/RandomizerViewModel.kt
+++ app/src/main/java/com/waether/app/features/randomizer/RandomizerViewModel.kt
@@ -1,20 +1,30 @@
 package com.waether.app.features.randomizer
 
 import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
+import eg.com.eserve.usecases.Ticker
 import eg.com.eserve.usecases.randomNumberGenerator
-import kotlin.random.Random
+
+private const val DEFAULT_VALUE = 0
+private const val INCREMENT_BY_VALUE = 1
 
 class RandomizerViewModel : ViewModel() {
 
-    val numberLiveData = MutableLiveData<Double>()
+    private val ticker = initializeTicker()
+    val numberLiveData = MutableLiveData<Int>()
+
+    private fun initializeTicker() = Ticker {
+        numberLiveData.postValue(randomNumberGenerator().toInt())
+    }
 
     init {
-        numberLiveData.value = randomNumberGenerator()
+        numberLiveData.value = DEFAULT_VALUE //randomNumberGenerator() // numberLiveData.postValue(1.0)
+        ticker.start()
+
     }
 
     override fun onCleared() {
         super.onCleared()
+        ticker.cancel()
     }
 }
